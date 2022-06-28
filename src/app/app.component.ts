a6d770f3339d22ff5f938c23bf8930b6e37dc624
Gustavo <gordono@unsa.edu.pe>
2022-06-27 21:53:22 -0500

    Workout in app root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = "my-dream-app";
+  name : string;
+  email;
+  webpage : string;
+  hobbies : string[];
+  showHobbies : boolean;
+  num : number;
+
+  constructor() {
+    console.log("Constructor Working...");
+    this.name = "Gustavo Eduardo ORDOÑO POMA";
+    this.email = "gordono@unsa.edu.pe";
+    this.webpage = "https://www.unsa.edu.pe";
+    this.hobbies = ["Fuchibol", "Programación", "Ajedrez"];
+    this.showHobbies = false;
+    this.num = 0;
+  }
+
+  toggleHobbies() {
+    this.showHobbies = !this.showHobbies;
+  }
+  newHobby(hobby: Hobby) {
+    // console.log(hobby.value);
+    if(hobby.value.trim() === "") {return false;}
+    this.hobbies.push(hobby.value);
+    hobby.value = "";
+    return false;
+  }
+  increment() {
+    this.num++;
+  }
+}
+
+interface Hobby {
+  value: string;
 }
